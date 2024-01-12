e0dbdd6192920c92742ab72e0105674cf1de6903
shiamsharif <shiam.sharif.07@gmail.com>
2024-01-12 21:55:48 +0600

    issue in header buttom burder commit

---
+++ src/js/nav-border-buttom.js
@@ -0,0 +1,13 @@
+// // scroll up js code 
+// const scrollHeader = () => {
+//     const header = document.getElementById("header");
+
+//     if (this.scrollY >= 50) {
+//         header.classList.remove("border-b", "border-secondaryColor");
+        
+//     } else {
+//         header.classList.add("border-b", "border-secondaryColor");
+//     }
+// };
+
+// window.addEventListener('scroll', scrollHeader);
